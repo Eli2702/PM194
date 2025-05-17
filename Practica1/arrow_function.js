c031bb8ce4a2e9ad14825c269090b0cef3dd289c
Eli2702 <122042377@upq.edu.mx>
2025-05-16 19:54:06 -0600

    Ejercicio_arrow_function_Practica1

---
+++ Practica1/arrow_function.js
@@ -0,0 +1,14 @@
+//funcion tradicional
+
+//function cuadrado(numero) {
+    //return numero * numero;
+//}
+
+
+//funcion arrow function
+const cuadrado = (numero) => numero * numero;
+
+console.log(cuadrado(5));
+console.log(cuadrado(10));
+console.log(cuadrado(22));
+
