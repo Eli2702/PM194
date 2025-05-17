d89eb83cd02a2a8500231bb8695088f0e18a2f45
Eli2702 <122042377@upq.edu.mx>
2025-05-16 19:44:52 -0600

    Ejemplo_let_const_Practica1

---
+++ Practica1/let_const.js
@@ -0,0 +1,13 @@
+// Ejemplo de uso de let y const en JavaScript
+
+
+//Cambiamos la variable nombre que estaba con var por let
+let nombre = "Armando";
+// Cambiamos la variable edad que estaba con var por const
+const edad = 25;
+
+nombre = "Ana María";
+
+const saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";
+
+console.log(saludo); // muestra el contenido de saludo
