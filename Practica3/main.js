24a556c8a7c9ebfd527f23dd9b3679b16d21a8e7
Eli2702 <122042377@upq.edu.mx>
2025-05-21 20:18:02 -0600

    EJERCICIO_1_PRACTICA3_UTILS_MAIN

---
+++ Practica3/main.js
@@ -0,0 +1,7 @@
+// Importar el archivo utils.js
+const restar = require('./utils');
+
+console.log(restar(10, 5));  
+console.log(restar(20, 30)); 
+console.log(restar(0, 0));   
+console.log(restar(7, 2));   
---
+++ Practica3/utils.js
@@ -0,0 +1,7 @@
+// Ejercicio 1: utils
+function restar(a, b) {
+  return a - b;
+}
+
+module.exports = restar;
+// exportar funcion, variable para que se pueda usar en otro archivo
