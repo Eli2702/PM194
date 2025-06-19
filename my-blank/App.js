2c5a13099f11bbc37d7600abfe83214f7fbbc53d
Eli2702 <122042377@upq.edu.mx>
2025-06-18 20:05:57 -0600

    Switch_EQUIPO_1

--- my-blank/App.js
+++ my-blank/App.js
@@ -3,28 +3,32 @@ import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View, Switch} from 'react-native';
 import React, { useState } from 'react';
 
-const Texto = ({style}) => {
-  const [contenido, setContenido] = useState('Hola Mundo RNative');
-  const actualizartexto = () => {setContenido('Estado actualizado del text')};
-  return (
-    <View Style={{margin: 10}}>
-     <Text Style={[style.text, style]}>{contenido}</Text>
-     <Button title='actualizartexto' onPress={actualizartexto}color="purple" />
-     </View>
-  );
+const Interruptor= () => {
+  const[isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+  return(
+    <View style={styles.container}>
+      <Text>
+        {isEnabled ? 'Activado': 'Desactivado'}
+      </Text>
+      <Switch
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+        />
+    </View>
+  )
+
 }
 
-const[isEnabled, setIsEnabled] = useState(false);
-const toggleSwitch = () => setIsEnabled(previousState => !previousState);
 
 /* Zona 2 : Main */
 export default function App() {
   return (
     <View style={styles.container}>
-      <Texto style={styles.red} />
-      <Texto style={styles.green} />
-      <Texto style={styles.yellow} />
-      <StatusBar style="auto" />
+    <Interruptor />
     </View>
   );
 }
